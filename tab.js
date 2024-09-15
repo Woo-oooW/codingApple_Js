9d5a484213bfbf8dc19d2ffefd90986ca0b42679
Woo <swy831@gmail.com>
2024-09-16 02:28:08 +0900

    modified:   tab.js

--- tab.js
+++ tab.js
@@ -1,3 +1,26 @@
-$('.tab-button').on('click',function(){
+for (let i = 0; i <(document.querySelectorAll('.tab-button').length); i++){
+
+    $('.tab-button').eq(i).on('click', function(){
+      $('.tab-button').removeClass('orange');
+      $('.tab-button').eq(i).addClass('orange');
+      $('.tab-content').removeClass('show');
+      $('.tab-content').eq(i).addClass('show');
+    })
+
+    // 바닐라
+    // document.querySelectorAll('.tab-button')[i].addEventListener('click',
+    //     function(){
+    //         document.querySelectorAll('.tab-button').forEach(element => {
+    //             element.classList.remove('orange');
+    //         })
+    //         document.querySelectorAll('.tab-content').forEach(element => {
+    //             element.classList.remove('show');
+    //         })
+    //         document.querySelectorAll('.tab-content')[i].classList.add('show');
+    //         document.querySelectorAll('.tab-button')[i].classList.add('orange');
+
+    //     }
+    // )
     
-})
+  
+  }
